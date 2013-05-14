d0b8e6b7ab3e0abb768f43482ed9d1c3a13b5b12
Jorge Prieto <jpheras@msn.com>
2013-05-14 22:33:07 +0200

    FORM2 index

--- ProgramacionEscorpiones/ProgramacionEscorpiones/Form1.cs
+++ ProgramacionEscorpiones/ProgramacionEscorpiones/Form1.cs
@@ -16,5 +16,10 @@ namespace ProgramacionEscorpiones
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
