aeadb5f882b1769a53525d2956c086670799caff
Eugenio Álvarez Muñoz <eugenioalvarezmunoz@gmail.com>
2013-05-19 18:55:35 +0200

    FORM 1 (INDEX) COMPLETADO
    ya entra en el form 3 (Principal) o en el 2 (registro)

--- ProgramacionEscorpiones/ProgramacionEscorpiones/Form1.cs
+++ ProgramacionEscorpiones/ProgramacionEscorpiones/Form1.cs
@@ -14,52 +14,66 @@ using MySql.Data.MySqlClient;
 namespace ProgramacionEscorpiones
 {
     public partial class Form1 : Form
-    {    //necesitamos unas variables 
-        //la linea que guarda el ip del servidor, el usuario y la pass
-        String cadenaConexion;
+    {
 
-        //conector que almacena la conexion a la BBDD
-        MySqlConnection conexion;
-
-        //necesitamos un comando para que se ejecute
-        MySqlCommand comando;
-
-        //Consulta
-        String sentenciaSQL;
-
-        //resultado de la consulta 
-        MySqlDataReader resultado;
-
-        //bool vUsuario, vPassword = true;
 
         public Form1()
         {
-
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
             try
             {
-                cadenaConexion = "Server = sql2.freesqldatabase.com ; Database = sql28127; Uid = sql28127; Pwd = lI9%vS2*; Port = 3306";
-                conexion = new MySqlConnection(cadenaConexion);
+                string cadenaConexion = "Server = sql2.freesqldatabase.com; Database = sql28127; Uid = sql28127; Pwd= lI9%vS2*; Port=3306";
+                MySqlConnection conexion = new MySqlConnection(cadenaConexion);
+                MySqlCommand comando = new MySqlCommand("SELECT * from sql28127.usuarios WHERE alias='" + this.textBox1.Text + "' AND pw='" + this.textBox2.Text + "'; ", conexion);
+                MySqlDataReader resultado;
                 conexion.Open();
+                resultado = comando.ExecuteReader();
+                int contador = 0;
+                while (resultado.Read())
+                {
+                    contador += 1;
+                    //abre la conexion y confirma la conexion y contraseña el contador sera 1, si esta repetida te pondra el contado a 2 
+                }
+                if (contador == 1)
+                {
+                    this.Hide();
+                    Form3 principal = new Form3();
+                    principal.ShowDialog();
+                }
+
+                else if (contador > 1)
+                {
+                    MessageBox.Show("DUPLICADO DE NOMBRE O CONTRASEÑA! LLAMAR A LOS GURÚS!");
+                }
+                else
+                    MessageBox.Show("Alias/Contraseña INCORRECTOS");
+                conexion.Close();
+
+
+
+
+
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-
-
+                MessageBox.Show(ex.Message);
             }
 
-            
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
 
-            Hide();
-          //  MessageBox.Show(""+textBox2.Text.ToString());  Muestra la contraseña, muestra que se oculta la pass pero no cambia de valor
+            this.Hide();
+            //  MessageBox.Show(""+textBox2.Text.ToString());  Muestra la contraseña, muestra que se oculta la pass pero no cambia de valor
             Form2 Registrar = new Form2();
-            Registrar.Show();
-           
+            Registrar.ShowDialog();
+
 
         }
 
@@ -70,31 +84,24 @@ namespace ProgramacionEscorpiones
 
         private void textBox2_Click(object sender, EventArgs e)
         {
-            
+
             // Se pone a blancos
             textBox2.Text = "";
             // Se muestra el texto escrito como asteriscos
-            textBox2.PasswordChar = '*';
+            textBox2.PasswordChar = '•';
             // Dejara un maximo de 30 caracteres al igual que la BBDD
             textBox2.MaxLength = 30;
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
 
-
-            Hide();
-            Form3 principal = new Form3();
-            principal.Show();
-        }
 
         private void Form1_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
 
-       
+
 
 
     }
