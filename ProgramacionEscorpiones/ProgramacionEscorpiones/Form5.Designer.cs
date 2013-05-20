c51680c18958afbd586e18892c7901a73f98508b
Jorge Prieto <jpheras@msn.com>
2013-05-20 11:45:35 +0200

    FORM5 TERMINADO
    excepto logo y comprobar

--- ProgramacionEscorpiones/ProgramacionEscorpiones/Form5.Designer.cs
+++ ProgramacionEscorpiones/ProgramacionEscorpiones/Form5.Designer.cs
@@ -32,6 +32,7 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
@@ -42,24 +43,27 @@
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "Nuevo Mail";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(50, 62);
+            this.textBox2.Location = new System.Drawing.Point(50, 88);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(180, 20);
             this.textBox2.TabIndex = 1;
             this.textBox2.Text = "Contraseña";
             this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.Click += new System.EventHandler(this.textBox2_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(102, 97);
+            this.button1.Location = new System.Drawing.Point(103, 125);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "Cambiar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -71,11 +75,22 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(50, 62);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(180, 20);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "Confirmar Mail";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.Click += new System.EventHandler(this.textBox3_Click);
+            // 
             // Form5
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(280, 141);
+            this.ClientSize = new System.Drawing.Size(285, 160);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
@@ -93,5 +108,6 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
--- ProgramacionEscorpiones/ProgramacionEscorpiones/Form5.cs
+++ ProgramacionEscorpiones/ProgramacionEscorpiones/Form5.cs
@@ -8,20 +8,100 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using MySql.Data.Types;
+using MySql.Data.MySqlClient;
+
 namespace ProgramacionEscorpiones
 {
     public partial class Form5 : Form
-    {
+    {   
+        Usuario usuario_activo = new Usuario();
+        //la linea que guarda la ip del servidor MySql, el usuario y la pass
+        String cadenaConexion;
+
+        //Conector que almacena la conexión a la BBDD
+        MySqlConnection conexion;
+
+        //comando que quiero que se ejecute
+        MySqlCommand comando;
+
+        //Consulta
+        String sentenciaSQL;
+
+        //resultado de la consulta
+        MySqlDataReader resultado;
         public Form5()
-        {
+        {   
+
             InitializeComponent();
+            try
+            {
+                cadenaConexion = cadenaConexion = "Server = sql2.freesqldatabase.com ; Database = sql28127; Uid = sql28127; Pwd = lI9%vS2*; Port = 3306"; 
+                conexion = new MySqlConnection(cadenaConexion);
+                conexion.Open();
+            }
+            catch (Exception)
+            {
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             Hide();
-            Form1 Index = new Form1();
-            Index.Show();
+            Form7 configuracion = new Form7();
+            configuracion.Show();
         }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            // Se pone a blancos
+            textBox2.Text = "";
+            // Se muestra el texto escrito como asteriscos
+            textBox2.PasswordChar = '•';
+            // Dejara un maximo de 30 caracteres al igual que la BBDD
+            textBox2.MaxLength = 30;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string correo1 = textBox1.Text.ToString();
+            string correo2 = textBox3.Text.ToString();
+            if (correo1 == correo2)
+            {
+                if (textBox1.Text == usuario_activo.getemail())
+                {
+                    sentenciaSQL = "UPDATE usuarios SET email=" + textBox1 + "WHERE id_usuario =" + usuario_activo.getemail() + ";";
+                    comando = new MySqlCommand(sentenciaSQL, conexion);
+                    resultado = comando.ExecuteReader();
+
+                }
+                else
+                {
+                    MessageBox.Show("este correo ya existe");
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("comprobar correo");
+            }
+        }
+                
+
+        
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void textBox3_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = "";
+        }
+
+      
+
+        
     }
 }
